7ddbd081b07bcdf1e863357486a409aaa3ff1a94
dtaylor4k <160911263+dtaylor4k@users.noreply.github.com>
2024-02-23 16:49:08 +0000

    Add files via upload

---
+++ netlify/functions/fetchBSC.js
@@ -0,0 +1,41 @@
+
+
+const Web3 = require('web3');
+const fetch = require("node-fetch");
+
+exports.handler = async (event) => {
+    const { walletAddress } = JSON.parse(event.body);
+    const apiKey = process.env.BSC_API_KEY;
+    const url = `https://api.bscscan.com/api?module=account&action=balance&address=${walletAddress}&apikey=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        if (result.status === "1") {
+            
+            const balance = Web3.utils.fromWei(result.result, 'ether');
+            const formattedBalance = parseFloat(balance).toFixed(4);
+
+          return {
+              statusCode: 200,
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ balance: formattedBalance })
+          };
+          
+        } else {
+            throw new Error('Failed to fetch balance');
+        }
+    } catch (error) {
+        return {
+          statusCode: 500,
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ error: 'Failed to fetch balance' })
+        };
+      
+    }
+};
