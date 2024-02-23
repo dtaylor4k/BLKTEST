866b4eb1b49724981bb39106718e721392b7ce8a
dtaylor4k <160911263+dtaylor4k@users.noreply.github.com>
2024-02-23 21:32:49 +0000

    Update fetchBSC.js

--- netlify/functions/fetchBSC.js
+++ netlify/functions/fetchBSC.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 exports.handler = async (event) => {
     const { walletAddress } = JSON.parse(event.body);
     const apiKey = process.env.BSC_API_KEY;
-    const url = `https://api.bscscan.com/api?module=account&action=balance&address=${walletAddress}&apikey=${apiKey}`;
+    const url = `https://api.bscscan.com/api?module=account&action=balance&address=${walletAddress}&apikey=UJUYX9GAMRPR5DUSWMVN97EDDW882CCJSH`;
 
     // Pre-define CORS headers
     const headers = {
